82bc4f22b8035f1ba8c7f4d88bf22fb60c74fd77
Rohithmarktricks <rohithbanka05@msitprogram.net>
2019-03-22 09:39:41 +0530

    activity-24

---
+++ BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MyReceiver.java
@@ -0,0 +1,14 @@
+package com.example.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String broadcastMessage = intent.getStringExtra("broadcast");
+        Toast.makeText(context, "Message: "+broadcastMessage, Toast.LENGTH_LONG).show();
+    }
+}
